c41770088d045ae490b14e2c09e594c5312e5bfc
Mayur Shingote <mayur.shingote@yahoo.com>
2020-05-31 08:35:22 +0530

    Initial version
    Initial version

---
+++ CancelSentInvitation.js
@@ -0,0 +1,8 @@
+//copy paste below code at in chrome console at https://www.linkedin.com/mynetwork/invitation-manager/sent/
+x='invitation-card__action-btn artdeco-button artdeco-button--muted artdeco-button--3 artdeco-button--tertiary ember-view'
+b=document.getElementsByClassName(x)
+for(var i in b){
+b[i].click();
+y='artdeco-modal__confirm-dialog-btn artdeco-button artdeco-button--2 artdeco-button--primary ember-view'
+document.getElementsByClassName(y)[0].click();
+}
